4e4e3f5a1de30804ad8fc30a795fe7624bff5082
git4robot <49167781@qq.com>
2017-07-18 11:19:22 +0800

    src_bak

---
+++ caffe-windows/src_ver_bak/caffe_windows.bat
@@ -0,0 +1,4 @@
+copy /Y .\caffe-win\caffe.proto ..\src\caffe\proto\caffe.proto
+copy /Y .\caffe-win\center_loss_layer.hpp ..\include\caffe\layers\center_loss_layer.hpp
+copy /Y .\caffe-win\center_loss_layer.cpp ..\src\caffe\layers\center_loss_layer.cpp
+copy /Y .\caffe-win\center_loss_layer.cu ..\src\caffe\layers\center_loss_layer.cu
